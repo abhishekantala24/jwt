cdff27644fb4992fbf4af7838dba558f8aae4956
Abhishek Antala <kumbhabhishek24@gmail.com>
2023-04-05 17:59:48 +0530

    message updated

---
+++ config/message.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "LOGIN_SUCCESS": "login successfully",
+    "SOMETHING_WRONG": "Oops! Something went wrong. !",
+    "CREDENTIAL_ERROR": "Email or Password is incorrect",
+    "USER_NOT_FOUND": "user not found",
+    "THANKYOU": "Thank you",
+    "WILL_CONTACT": "will will contact you soon",
+}
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
+const Message = require("../config/message")
+
 const dbUser = require('../modals/createuser')
 const dbFeedback = require('../modals/feedback')
 const dbInquiry = require('../modals/inquiry')
-
 
 module.exports.home = (req, resp) => {
     resp.send('home')
@@ -10,26 +11,26 @@ module.exports.home = (req, resp) => {
 module.exports.createUser = async (req, resp) => {
     const { email, password, phone, roll, name } = req.body
     try {
-        const user = await dbUser.create({ email, password, phone, roll, name })
+        await dbUser.create({ email, password, phone, roll, name })
         resp.status(201).send("user created")
     }
     catch (err) {
-        resp.status(400).send("oops! something went wrong")
+        resp.status(400).send(Message.SOMETHING_WRONG)
     }
 }
 
 module.exports.login_email = async (req, resp) => {
     const { email, password } = req.body
     try {
-        const user = await dbUser.find({ email, password })
+        await dbUser.find({ email, password })
         if (user.length > 0) {
-            resp.status(201).send("login successfully")
+            resp.status(201).send(Message.LOGIN_SUCCESS)
         } else {
-            resp.status(400).send("invalid password or email")
+            resp.status(400).send(Message.CREDENTIAL_ERROR)
         }
     }
     catch (err) {
-        resp.status(400).send("user not found")
+        resp.status(400).send(Message.USER_NOT_FOUND)
     }
 }
 
@@ -37,10 +38,10 @@ module.exports.feedback = async (req, resp) => {
     const { name, phone, message } = req.body
     try {
         await dbFeedback.create({ name, phone, message })
-        resp.status(201).send("Thank you")
+        resp.status(201).send(Message.THANKYOU)
     }
     catch (err) {
-        resp.status(400).send("oops! something went wrong ")
+        resp.status(400).send(Message.USER_NOT_FOUND)
     }
 }
 
@@ -48,9 +49,9 @@ module.exports.inquiry = async (req, resp) => {
     const { name, phone, reason } = req.body
     try {
         await dbInquiry.create({ name, phone, reason })
-        resp.status(201).send("will will contact you soon")
+        resp.status(201).send(Message.WILL_CONTACT)
     }
     catch (err) {
-        resp.status(400).send("oops! something went wrong ")
+        resp.status(400).send(Message.USER_NOT_FOUND)
     }
 }
